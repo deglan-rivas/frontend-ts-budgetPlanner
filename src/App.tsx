67d9e8b459785ba389c4de29779902ab62e83577
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-29 21:57:04 -0500

    feat: :sparkles: add expenseList section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import BudgetTracker from "./components/BudgetTracker"
 import ExpenseFilter from "./components/ExpenseFilter"
+import ExpenseList from "./components/ExpenseList"
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-200 space-y-10">
+    <div className="min-h-screen bg-gray-200">
       <h1 className="bg-blue-600 text-white uppercase text-4xl font-semibold py-8 text-center">
         Planificador de Gastos
       </h1>
 
       {/* <BudgetForm /> */}
 
-      <BudgetTracker />
-      <ExpenseFilter />
+      <div className="space-y-10 py-10">
+        <BudgetTracker />
+        <ExpenseFilter />
+        <ExpenseList />
+      </div>
 
     </div>
   )
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,71 @@
+const expenseList = [
+  {
+    id: "abc123",
+    category: "ahorro",
+    name: "gasto ahorro",
+    quantity: 12,
+    date: "lunes, 29 de abril de 2024",
+  },
+  {
+    id: "def456",
+    category: "casa",
+    name: "gasto casa",
+    quantity: 21,
+    date: "lunes, 30 de abril de 2024",
+  }
+]
+
+function ExpenseItem({ expense }) {
+  const { category, name, quantity, date } = expense
+  return (
+    <div className="flex justify-between items-center gap-5">
+      <div className="flex items-center gap-5">
+        <img src={`/icono_${category}.svg`} alt="ahorro" className="w-20" />
+        <div className="space-y-2">
+          <p className="uppercase text-gray-500 font-semibold text-sm">
+            {category}
+          </p>
+          <p>
+            {name}
+          </p>
+          <p className="text-sm text-gray-500">
+            {date}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <p className="text-2xl font-semibold">
+          ${quantity.toFixed(2)}
+        </p>
+        <p className="bg-blue-600 text-white text-center font-semibold cursor-pointer">
+          Update
+        </p>
+        <p className="bg-rose-600 text-white text-center font-semibold cursor-pointer">
+          Delete
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default function ExpenseList() {
+  return (
+    <section className="px-10 py-10 bg-white max-w-3xl mx-auto space-y-10">
+      <h2 className="text-gray-600 font-semibold text-2xl">
+        Listado de Gastos
+      </h2>
+
+      {
+        expenseList.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            expense={expense}
+          />
+        ))
+      }
+
+
+    </section>
+  )
+}
