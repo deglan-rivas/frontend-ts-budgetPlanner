5a8c9a3ba412340b1d8bfa8d7a39b0d24ab9c5b4
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-29 23:58:03 -0500

    feat: :sparkles: add budget context

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,22 +1,22 @@
-// import { BudgetActions, budgetReducer, BudgetState, initialState } from '@/reducers/budget-reducer';
-// import { createContext, ReactNode, useReducer } from 'react';
+import { BudgetActions, budgetReducer, BudgetState, initialState } from '@/reducers/budget-reducer';
+import { createContext, ReactNode, useReducer } from 'react';
 
-// type BudgetContextProps = {
-//   state: BudgetState
-//   dispatch: React.Dispatch<BudgetActions>
-// }
+type BudgetContextProps = {
+  state: BudgetState
+  dispatch: React.Dispatch<BudgetActions>
+}
 
-// export const BudgetContext = createContext<BudgetContextProps>(null!)
+export const BudgetContext = createContext<BudgetContextProps>(null!)
 
-// export const BudgetProvider = ({ children }: { children: ReactNode }) => {
-//   const [state, dispatch] = useReducer(budgetReducer, initialState)
+export const BudgetProvider = ({ children }: { children: ReactNode }) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState)
 
-//   return (
-//     <BudgetContext.Provider value={{
-//       state,
-//       dispatch
-//     }}>
-//       {children}
-//     </BudgetContext.Provider>
-//   )
-// }
+  return (
+    <BudgetContext.Provider value={{
+      state,
+      dispatch
+    }}>
+      {children}
+    </BudgetContext.Provider>
+  )
+}
